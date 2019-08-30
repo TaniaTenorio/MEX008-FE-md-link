efefcad0cf106689d163b44d3f8ed3b49435f8dd
TaniaTenorio <taniatenorio@hotmail.com>
2019-08-30 17:08:36 -0500

    se crea la funcion para leer el archivo

--- readFile.js
+++ readFile.js
@@ -1,10 +1,11 @@
 const fs = require("fs");
 
-const readFile = fs.readFile("./README.md", "utf8", (err,data) => {
+const readFile = ()=>{
+    fs.readFile("./README.md", "utf8", (err,data) => {
     if(err) throw err;
     const mdFile = data;
     console.log(mdFile);
     return mdFile
 });
-
+}
 module.exports = readFile;
