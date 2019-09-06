bf9feda9c3ade1f88fc6d7d685ac694dea0f197b
TaniaTenorio <taniatenorio@hotmail.com>
2019-09-06 17:21:56 -0500

    se importan los modulos readFile y findLink

--- index.js
+++ index.js
@@ -2,8 +2,16 @@
 const readFile = require("./readFile.js");
 const findLInk = require("./findLink.js");
 
-
-const mdlinks = (file, option) =>{
-
-} 
+const mdlinks = (file, options) =>{
+    readFile(file).then((data) => {
+    console.log("esta es la data", data);
+    return findLInk(data);
+    })
+        .then((linkData) => {
+            console.log(linkData);
+        });
+  // if (file != null){
+  //   console.log("tenemos archivo para leer", file);
+  //   }
+};
 module.exports = mdlinks;
