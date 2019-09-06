4a6b0a27ab2f272df49c40d163abeadd233358b5
TaniaTenorio <taniatenorio@hotmail.com>
2019-09-06 00:08:51 -0500

    se crea una promesa que lee el archivo y retorna el contenido en string

--- readFile.js
+++ readFile.js
@@ -1,14 +1,15 @@
 const fs = require("fs");
+const path = require ("path");
+//console.log(process.argv[2]);
 
 const readFile = () => {
     const fileContent = new Promise((resolve, reject) => {
-        fs.readFile("./testingFile.md", "utf8", (err,data) => {
+        fs.readFile(path.resolve("./testingFile.md"), "utf8", (err,data) => {
             if(err) return reject(err);
             resolve(data);
         })
     })
     return fileContent
-
 }
 //readFile();
 module.exports = readFile;
