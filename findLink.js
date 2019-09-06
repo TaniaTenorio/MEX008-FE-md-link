ac8e7e39a42de6711a69f48801539e60f0017295
TaniaTenorio <taniatenorio@hotmail.com>
2019-09-06 17:20:58 -0500

    se crea la promesa para encontrar los links y se agrega al objeto la ruta del archivo que se evalua

--- findLink.js
+++ findLink.js
@@ -1,27 +1,31 @@
 const readFile = require("./readFile.js");
+const path = require ("path");
 
 
-// console.log(readFile());
-readFile().then((data) =>{
-    const regEx = /\[([a-zA-Z0-9\.]{1,})+\]\((https?\:\/\/(www\.)?([a-zA-Z0-9\-\_]{1,})+((\.?[a-zA-Z0-9\-\_\$\%\=\#\?\&]{1,4})?\/?){1,})\)/gm;
-    const linksArray = [];
-    let href;
-    let url;
-    while ((link = regEx.exec(data)) !== null) {
-    const linkInfo = new Object();
-    linkInfo.href = link[1];
-    linkInfo.url = link[2];
-    linksArray.push(linkInfo);
+    const findLink = (data) => {
+    const linkData = new Promise((resolve, reject)=> {
+        const regEx = /\[([a-zA-Z0-9\.]{1,})+\]\((https?\:\/\/(www\.)?([a-zA-Z0-9\-\_]{1,})+((\.?[a-zA-Z0-9\-\_\$\%\=\#\?\&]{1,4})?\/?){1,})\)/gm;
+        const linksArray = [];
+        let link;
+        // let href;
+        // let url;
+        while ((link = regEx.exec(data)) != null) {
+            const linkInfo = new Object();
+            linkInfo.href = link[1];
+            linkInfo.text = link[2];
+            linkInfo.file = path.resolve();
+            linksArray.push(linkInfo);
+            console.log(linksArray);
+            
+            resolve(linksArray);
+        }
+        if(linksArray == null){
+            const err = new Error ("No se encontraron links en este archivo");
+            return reject(err);
+        }
+})
+return linkData;
 }
-console.log(linksArray);
-});
-// const fileContent = readFile();
-// console.log(fileContent);
+    // console.log(linksArray);
 
-
-// const link = "https://www.google.com, https://www.facebook.com, https://www.hotmail.com";
-
-// const linkStructure = /https/g;
-// const linkMatch = fileContent.match(linkStructure);
-// console.log(linkMatch);
-
+module.exports = findLink;
