22f75ca1b37b5c395b178cfc88653b67b2da97b7
TaniaTenorio <taniatenorio@hotmail.com>
2019-09-06 00:07:48 -0500

    se crea la regEx y el array donde se muestran el href y el url

--- findLink.js
+++ findLink.js
@@ -3,9 +3,18 @@ const readFile = require("./readFile.js");
 
 // console.log(readFile());
 readFile().then((data) =>{
-    console.log(typeof data);
-    
-} )
+    const regEx = /\[([a-zA-Z0-9\.]{1,})+\]\((https?\:\/\/(www\.)?([a-zA-Z0-9\-\_]{1,})+((\.?[a-zA-Z0-9\-\_\$\%\=\#\?\&]{1,4})?\/?){1,})\)/gm;
+    const linksArray = [];
+    let href;
+    let url;
+    while ((link = regEx.exec(data)) !== null) {
+    const linkInfo = new Object();
+    linkInfo.href = link[1];
+    linkInfo.url = link[2];
+    linksArray.push(linkInfo);
+}
+console.log(linksArray);
+});
 // const fileContent = readFile();
 // console.log(fileContent);
 
